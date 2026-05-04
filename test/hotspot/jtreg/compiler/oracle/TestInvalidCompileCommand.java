67b48ae1a128dcfedceede3a6064c23eacdcdfef
Kirill Shirokov <kirshiro@amazon.com>
2026-05-04 15:34:40 -0700

    Fix -XX:CompileCommand=MemStat-related tests. Fix tested invalid values for -XX:CompileCommand compilation levels

--- test/hotspot/jtreg/compiler/oracle/TestInvalidCompileCommand.java
+++ test/hotspot/jtreg/compiler/oracle/TestInvalidCompileCommand.java
@@ -72,7 +72,7 @@ public class TestInvalidCompileCommand {
             "Missing type 'intx' before option 'RepeatCompilation'"
         },
         {
-            "Missing type 'uintx' before option 'MemStat'"
+            "Missing type 'uint' before option 'MemStat'"
         },
         {
             "Missing type 'ccstrlist' before option 'ControlIntrinsic'"
